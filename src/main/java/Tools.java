014e99d5e7f025a5260f2630da67f5bc4cd7c0ea
RiceBallBall <foxcaoruiadt@icloud.com>
2020-09-12 17:27:20 +0800

    change_des_tools

--- src/main/java/Tools.java
+++ src/main/java/Tools.java
@@ -3,43 +3,54 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Tools {
-    public String getTS(){
+    public String getTS() {
         SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");//设置日期格式
         String date = df.format(new Date());// new Date()为获取当前系统时间，也可使用当前时间戳
         return date;
     }
-    public String[] splite_name(String names){
-        String name[]=names.split("#");
+
+    public String[] splite_name(String names) {
+        String name[] = names.split("#");
         return name;
     }
-    public String[] dataSplite(String longdata,int n){
-        String data[]=new String[n];
-        for (int i=0;i<n;i++){
-            data[i]=longdata.substring(i*64,i*64+64);
+
+    public String[] dataSplite(String longdata, int len) {//数据，单个数据长度
+        int n = longdata.length() / len;
+        if (longdata.length() % len != 0) {
+            n += 1;
         }
-        return data;
+        String data[] = new String[n];
+        for (int i = 0; i < n; i++) {
+            if (i != n - 1) {
+                data[i] = longdata.substring(i * len, (i + 1) * len);
+            } else {
+                data[i] = longdata.substring(i * len);
+            }
+        }
+        return data;//返回数组
 
     }
+
     static public String FileIn(String f) throws IOException {//本地文件读到字符串
 //        f = "C:\\Users\\caorui\\Pictures\\ps\\DSC_0649.jpg";
 //        save="C:\\Users\\caorui\\Pictures\\ps\\a.txt";
-        File file=new File(f);
+        File file = new File(f);
 
         InputStream is = new FileInputStream(file);
         try {
             byte[] bb = new byte[256];
             int ch;
             ch = is.read(bb);
-            String str="";
+            String str = "";
             while (ch != -1) {
-                str+= conver2HexStr(bb);
+                str += conver2HexStr(bb);
                 ch = is.read(bb);
             }
             String s = "";
             for (int j = 0; j < str.length() / 8; j++) {
                 String t1 = str.substring(j * 8, j * 8 + 8);
-                    int temp = Integer.valueOf(t1, 2);
-                    s = s + (char) temp;
+                int temp = Integer.valueOf(t1, 2);
+                s = s + (char) temp;
 
             }
             return s;
@@ -51,9 +62,9 @@ public class Tools {
     }
 
     static public void FileOut(String filePath, String fileStr) {//从字符串存到本地
-        String fileBin="";
+        String fileBin = "";
         char[] strChar = fileStr.toCharArray();
-        String  temp;
+        String temp;
         for (int i = 0; i < strChar.length; i++) {
             temp = String.format("%08d", Integer.parseInt(Integer.toBinaryString(strChar[i])));
             fileBin += temp;
@@ -70,9 +81,9 @@ public class Tools {
             try {
                 FileOutputStream fileOutputStream = new FileOutputStream(filePath);
 //                    System.out.println("==="+text.length());
-                    byte b[] = conver2HexToByte(fileBin);
-                    fileOutputStream.write(b);
-                    fileOutputStream.flush();
+                byte b[] = conver2HexToByte(fileBin);
+                fileOutputStream.write(b);
+                fileOutputStream.flush();
 
                 fileOutputStream.close();
             } catch (Exception e) {
@@ -80,19 +91,21 @@ public class Tools {
             }
         }
     }
+
     static public String conver2HexStr(byte[] b) {
         StringBuffer result = new StringBuffer();
         for (int i = 0; i < b.length; i++) {
-            result.append(String.format("%08d",Integer.parseInt(Long.toString(b[i] & 0xff, 2))));
+            result.append(String.format("%08d", Integer.parseInt(Long.toString(b[i] & 0xff, 2))));
 //            result.append(Long.toString(b[i] & 0xff, 2)+",");
 
         }
         return result.toString().substring(0, result.length());
     }
+
     static public byte[] conver2HexToByte(String hex2Str) {
-        int max=hex2Str.length()/8;
+        int max = hex2Str.length() / 8;
         String[] temp = new String[max];
-        for (int i=0;i<max;i++){
+        for (int i = 0; i < max; i++) {
             temp[i] = hex2Str.substring(i * 8, i * 8 + 8);
         }
         byte[] b = new byte[temp.length];
@@ -103,9 +116,9 @@ public class Tools {
     }
 
     public static void main(String[] args) throws IOException {
-        String s=Tools.FileIn("/Users/katherine/Desktop/Study/IOTS/iots.cr/src/main/java/my.pdf");
-        System.out.println("["+s+"]");
-        Tools.FileOut("/Users/katherine/Desktop/Study/IOTS/iots.cr/src/main/java/may.pdf",s);
+        String s = Tools.FileIn("/Users/katherine/Desktop/Study/IOTS/iots.cr/src/main/java/my.pdf");
+        System.out.println("[" + s + "]");
+        Tools.FileOut("/Users/katherine/Desktop/Study/IOTS/iots.cr/src/main/java/may.pdf", s);
 //        String s=DES.encode("12345678","123456789");
 //        System.out.println("["+s+"]");
 //
