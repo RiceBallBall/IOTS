08b7ff8764ea3cc2c0fa1b7a00e0813c1e0c71bd
shaddocks <45387682+shaddocks@users.noreply.github.com>
2020-09-14 11:38:17 +0800

    Update ServerDisk.java

--- src/main/java/server/ServerDisk.java
+++ src/main/java/server/ServerDisk.java
@@ -1,1 +1,35 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+public class ServerDisk {
+    private static int peopleNumber = 8;
+    private static int port = 9999;
+    private static ExecutorService serviceThread = Executors.newFixedThreadPool(peopleNumber);
+
+    public static void main(String[] args){
+        try {
+            ServerSocket server = new ServerSocket(port);
+            Socket socket = null;
+            while (true) {
+                socket = server.accept();
+                Authentication task = new Authentication(socket);
+                try{
+                    Future future = serviceThread.submit(task);
+                    String feedback = future.get().toString();
+                    if(!feedback.equals("false")){
+                        Disk disk = new Disk(socket, feedback.split(" ")[0], feedback.split(" ")[1]);
+                        serviceThread.submit(disk);
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+            // serviceThread.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
