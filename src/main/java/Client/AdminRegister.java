79716a54a1d673ef78b1141e75c4cdd46c0fe6ec
zch1254746725 <45356453+zch1254746725@users.noreply.github.com>
2020-09-15 11:34:08 +0800

    Update AdminRegister.java

--- src/main/java/Client/AdminRegister.java
+++ src/main/java/Client/AdminRegister.java
@@ -1,159 +1,132 @@
 package Client;
 
-import AS.Tools;
-
-import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import javax.swing.*;
 
-public class Function {
-    public ServerClient myClient;
-    public Socket socket;
-    InputStreamReader inputStreamReader;
-    BufferedReader bufferedReader;
-    PrintWriter writer;
-    JButton btn01 = new JButton("文件上传");
-    JButton btn02 = new JButton("文件下载/删除");
-    JButton btn03 = new JButton("退出系统");
-
-    Function(ServerClient client, Socket as_socket, BufferedReader bufferedReader, PrintWriter writer) throws IOException {
-        //init();
-        socket = as_socket;
-        this.bufferedReader = bufferedReader;
-        this.writer = writer;
-        myClient = client;
-        JFrame jf = new JFrame();
-        //窗口名称
-        jf.setTitle("文件传输系统");
-        //弹窗大小
-        jf.setSize(400, 600);
-        //居中显示
-        jf.setLocationRelativeTo(null);
-        //设置进程退出方法
-        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        //流式布局管理器
-        JPanel panel = new JPanel(new FlowLayout());
-        //创建标题
-        JLabel label = new JLabel("功能选择");
-        label.setOpaque(true);
-
-        //设置按钮
-        btn01.setBounds(100, 150, 100, 50);
-        btn02.setBounds(100, 200, 100, 50);
-        btn03.setBounds(150, 250, 100, 50);
-        // inputStreamReader = new InputStreamReader(socket.getInputStream());
-        // bufferedReader = new BufferedReader(inputStreamReader);
-        panel.add(btn01);
-        panel.add(btn02);
-        panel.add(btn03);
-
-        jf.setContentPane(panel);
-        jf.setVisible(true);
+public class AdminRegister {
+    String AS_IP = "172.20.10.2";
+    AdminRegister(ServerClient myClient, Socket mysocket) {
+        socket=mysocket;
+        init();
+        client = myClient;
     }
 
-    //public Client client;
-    public void run() throws IOException {
-        //窗口类
-        //设置监听
-        btn01.addActionListener(new ActionListener() {
+    public ServerClient client;
+    public  Socket socket;
+
+    void init() {
+
+        JFrame frame = new JFrame("注册账号");
+        frame.setLayout(null);
+
+        JLabel nameStr = new JLabel("用户名:");
+        nameStr.setBounds(250, 150, 100, 25);
+        frame.add(nameStr);
+
+        JLabel IDStr = new JLabel("账号:");
+        IDStr.setBounds(250, 200, 100, 25);
+        frame.add(IDStr);
+
+        JLabel passwordStr = new JLabel("密码:");
+        passwordStr.setBounds(250, 250, 100, 25);
+        frame.add(passwordStr);
+
+        JLabel confrimStr = new JLabel("确认密码:");
+        confrimStr.setBounds(250, 300, 100, 30);
+        frame.add(confrimStr);
+
+        JTextField userName = new JTextField();
+        userName.setBounds(320, 150, 150, 25);
+        frame.add(userName);
+
+        JTextField userID = new JTextField();
+        userID.setBounds(320, 200, 150, 25);
+        frame.add(userID);
+
+        JPasswordField password = new JPasswordField();
+        password.setBounds(320, 250, 150, 25);
+        frame.add(password);
+
+        JPasswordField confrimPassword = new JPasswordField();
+        confrimPassword.setBounds(320, 300, 150, 25);
+        frame.add(confrimPassword);
+
+        JButton buttonregister = new JButton("注册");
+        buttonregister.setBounds(350, 350, 70, 25);
+        frame.add(buttonregister);
+
+
+        frame.setBounds(400, 100, 800, 640);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        //为注册按钮增加监听器
+        buttonregister.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // 获取到的事件源就是按钮本身
-                // JButton btn = (JButton) e.getSource();
-                FileUP fileup = new FileUP(socket, bufferedReader, writer, myClient);
-                fileup.run();
-                System.out.println("选择进行文件上传");
-            }
-        });
-
-        btn02.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // 获取到的事件源就是按钮本身
-                JButton btn = (JButton) e.getSource();
-                String Basic[];
-                String data[];
-                //socket连接Server端(刷新目录)
-
-                myClient.ClientAction("9", socket, bufferedReader, writer);
-                String pack="";
-                System.out.println("pack:"+pack);
-                try {
-                    pack = bufferedReader.readLine();
+                String name = userName.getText();
+                client.ID_c = userID.getText();
+                client. setK_c(new String(password.getPassword()));
+//                String confrimpasswd = new String(confrimPassword.getPassword());
+//                socket连接AS
+                String pack ="";
+                try (InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+                     BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                     PrintWriter writer = new PrintWriter(socket.getOutputStream()))
+                {
+                    client.ClientAction("7", socket, bufferedReader, writer);
+                    pack=bufferedReader.readLine();
+                    System.out.println(pack);
+                    boolean j= client.verify_m(pack);
+                    System.out.println(j);
+                    while (j!=true){
+                        client.ClientAction("7",socket, bufferedReader, writer);
+                        j= client.verify_m(pack);
+                    }
+                    boolean fb=client.Clientexcecution(pack);
+                    if(fb){
+                        frame.setVisible(false);
+//                        frame.dispose();
+                        Login_Register login_register = new Login_Register(client);
+                    }else{
+                        JOptionPane.showMessageDialog(null,"注册失败，无法注册","Error", JOptionPane.ERROR_MESSAGE);
+                        //失败弹窗
+                    }
                 } catch (IOException ioException) {
                     ioException.printStackTrace();
                 }
-                boolean j = false;
-                //等待Server回复
-//                while (!j) {//10号反馈包的验证
-                    //等待回复 readline阻塞
-                    j = myClient.verify_m(pack);
-//                    if (!j) {//刷新请求失败则重新请求
-//                        myClient.ClientAction("9", socket, bufferedReader, writer);
-//                    }
-//                }
-                try {
-                    pack = bufferedReader.readLine();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
+                try(InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    PrintWriter writer = new PrintWriter(socket.getOutputStream()))
+                    {
+                        client.ClientAction("7",socket, bufferedReader, writer);
+                    }catch (IOException e1){
+                    e1.printStackTrace();
                 }
-                String Basic_info[] = myClient.Divide(pack);
-                data = myClient.m10_d(Basic_info[6], myClient.Kc_v);
-                String sp[] = Tools.splite_name(data[1]);
-                String display = "";
-                for (int i = 0; i < sp.length; i++) {
-                    display += sp[i] + "\n";
-                }
-                FileDOWN filedown = new FileDOWN("文件传输系统--文件下载", myClient, socket, bufferedReader, writer, display);
-                System.out.println("选择进行文件下载");
-            }
-        });
+                // client.ClientAction("7",socket);
+//                System.out.println();
+//                监听得到AS的反馈
 
-        btn03.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //获取到的事件源就是按钮本身
-                JButton btn = (JButton) e.getSource();
-                String Basic[];
-                String data[];
-                //socket连接AS(离线提醒)
-                myClient.ClientAction("23a", socket, bufferedReader, writer);
-                String pack="";
-                boolean j = false;
-                //等待AS端回复
-                while (!j) {
-                    //等待回复 readline阻塞
-                    j = myClient.verify_m(pack);
-                    if (!j) {//离线请求失败则重新请求
-                        myClient.ClientAction("23a", socket, bufferedReader, writer);
-                    }
-                }
-                myClient.Clientexcecution(pack);
-                //断开AS连接Server
-                myClient.ClientAction("23s", socket, bufferedReader, writer);
-                //等待Server端回复
-                while (!j) {
-                    //等待回复 readline阻塞
-                    j = myClient.verify_m(pack);
-                    if (!j) {//离线请求失败则重新请求
-                        myClient.ClientAction("23s", socket, bufferedReader, writer);
-                    }
-                }
-                boolean ser_v = myClient.Clientexcecution(pack);
-                if (ser_v) {
-                    JOptionPane.showMessageDialog(null, "成功退出系统，该账号已离线", "Remind", JOptionPane.INFORMATION_MESSAGE);
-                } else {
-                    JOptionPane.showMessageDialog(null, "离线申请失败", "Error", JOptionPane.ERROR_MESSAGE);
-                }
-                System.out.println("退出系统");
             }
-        });
 
+
+                //创建Register类
+//                Reginster register = new Reginster();
+//                register.setID();
+//                register.setName(name);
+//                register.setPassword(passwd);
+//                register.setconfirmpasswd(confrimpasswd);
+
+                //如果注册成功，返回登录界面
+
+
+
+            });
     }
 }
