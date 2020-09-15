559d91b05e5a11e7b22ed1327c595101c5562ccc
zch1254746725 <45356453+zch1254746725@users.noreply.github.com>
2020-09-15 11:33:29 +0800

    Update Function.java

--- src/main/java/Client/Function.java
+++ src/main/java/Client/Function.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Function {
@@ -16,15 +17,18 @@ public class Function {
     public Socket socket;
     InputStreamReader inputStreamReader;
     BufferedReader bufferedReader;
+    PrintWriter writer;
     JButton btn01 = new JButton("文件上传");
     JButton btn02 = new JButton("文件下载/删除");
     JButton btn03 = new JButton("退出系统");
 
-    Function(ServerClient client, Socket as_socket) throws IOException {
+    Function(ServerClient client, Socket as_socket, BufferedReader bufferedReader, PrintWriter writer) throws IOException {
         //init();
         socket = as_socket;
+        this.bufferedReader = bufferedReader;
+        this.writer = writer;
         myClient = client;
-        javax.swing.JFrame jf = new javax.swing.JFrame();
+        JFrame jf = new JFrame();
         //窗口名称
         jf.setTitle("文件传输系统");
         //弹窗大小
@@ -43,8 +47,8 @@ public class Function {
         btn01.setBounds(100, 150, 100, 50);
         btn02.setBounds(100, 200, 100, 50);
         btn03.setBounds(150, 250, 100, 50);
-        inputStreamReader = new InputStreamReader(socket.getInputStream());
-        bufferedReader = new BufferedReader(inputStreamReader);
+        // inputStreamReader = new InputStreamReader(socket.getInputStream());
+        // bufferedReader = new BufferedReader(inputStreamReader);
         panel.add(btn01);
         panel.add(btn02);
         panel.add(btn03);
@@ -62,7 +66,7 @@ public class Function {
             public void actionPerformed(ActionEvent e) {
                 // 获取到的事件源就是按钮本身
                 // JButton btn = (JButton) e.getSource();
-                FileUP fileup = new FileUP();
+                FileUP fileup = new FileUP(socket, bufferedReader, writer, myClient);
                 fileup.run();
                 System.out.println("选择进行文件上传");
             }
@@ -77,8 +81,9 @@ public class Function {
                 String data[];
                 //socket连接Server端(刷新目录)
 
-                myClient.ClientAction("9", socket);
-                String pack= null;
+                myClient.ClientAction("9", socket, bufferedReader, writer);
+                String pack="";
+                System.out.println("pack:"+pack);
                 try {
                     pack = bufferedReader.readLine();
                 } catch (IOException ioException) {
@@ -86,13 +91,13 @@ public class Function {
                 }
                 boolean j = false;
                 //等待Server回复
-                while (!j) {//10号反馈包的验证
+//                while (!j) {//10号反馈包的验证
                     //等待回复 readline阻塞
                     j = myClient.verify_m(pack);
-                    if (!j) {//刷新请求失败则重新请求
-                        myClient.ClientAction("9", socket);
-                    }
-                }
+//                    if (!j) {//刷新请求失败则重新请求
+//                        myClient.ClientAction("9", socket, bufferedReader, writer);
+//                    }
+//                }
                 try {
                     pack = bufferedReader.readLine();
                 } catch (IOException ioException) {
@@ -105,7 +110,7 @@ public class Function {
                 for (int i = 0; i < sp.length; i++) {
                     display += sp[i] + "\n";
                 }
-                FileDOWN filedown = new FileDOWN("文件传输系统--文件下载", myClient, socket, display);
+                FileDOWN filedown = new FileDOWN("文件传输系统--文件下载", myClient, socket, bufferedReader, writer, display);
                 System.out.println("选择进行文件下载");
             }
         });
@@ -118,7 +123,7 @@ public class Function {
                 String Basic[];
                 String data[];
                 //socket连接AS(离线提醒)
-                myClient.ClientAction("23a", socket);
+                myClient.ClientAction("23a", socket, bufferedReader, writer);
                 String pack="";
                 boolean j = false;
                 //等待AS端回复
@@ -126,18 +131,18 @@ public class Function {
                     //等待回复 readline阻塞
                     j = myClient.verify_m(pack);
                     if (!j) {//离线请求失败则重新请求
-                        myClient.ClientAction("23a", socket);
+                        myClient.ClientAction("23a", socket, bufferedReader, writer);
                     }
                 }
                 myClient.Clientexcecution(pack);
                 //断开AS连接Server
-                myClient.ClientAction("23s", socket);
+                myClient.ClientAction("23s", socket, bufferedReader, writer);
                 //等待Server端回复
                 while (!j) {
                     //等待回复 readline阻塞
                     j = myClient.verify_m(pack);
                     if (!j) {//离线请求失败则重新请求
-                        myClient.ClientAction("23s", socket);
+                        myClient.ClientAction("23s", socket, bufferedReader, writer);
                     }
                 }
                 boolean ser_v = myClient.Clientexcecution(pack);
